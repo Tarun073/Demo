5e5abaa956a14638925c067102faf0bebe290392
Tarun073 <tarun.gowda073@gmail.com>
2025-02-18 15:26:14 +0530

    ADD

---
+++ Demo161.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demo161;
+
+/**
+ *
+ * @author ise
+ */
+public class Demo161 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hi");
+        System.out.println("Hi");
+        // TODO code application logic here
+    }
+
+}
